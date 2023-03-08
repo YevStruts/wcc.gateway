d388ff7e1d6f5edd7ca356ab1e2cefdf9f06f65f
Yev <str.lucky86@gmail.com>
2023-03-08 12:41:49 -0500

    added displaying of participants for tournament.

--- wcc.gateway.kernel/Models/TournamentModel.cs
+++ wcc.gateway.kernel/Models/TournamentModel.cs
@@ -7,6 +7,7 @@
         public string? Description { get; set; }
         public string? Image_url { get; internal set; }
         public int Count_players { get; set; }
+        public List<PlayerModel> Participant { get; set; }
         public DateTime Date_start { get; set; }
         public DateTime Date_created { get; set; }
     }
