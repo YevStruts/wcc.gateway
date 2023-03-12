fdcfc1c6364b63091bd8e260a5f878853c6b6fb6
Yev <str.lucky86@gmail.com>
2023-03-11 21:21:39 -0500

    added supporting of avatar null.

--- wcc.gateway.integrations/Discord/Helpers/DiscordHelper.cs
+++ wcc.gateway.integrations/Discord/Helpers/DiscordHelper.cs
@@ -10,6 +10,8 @@ namespace wcc.gateway.integrations.Discord.Helpers
     {
         public static string GetAvatarUrl(string userId, string avatar)
         {
+            if (string.IsNullOrEmpty(avatar))
+                return string.Empty;
             return $"https://cdn.discordapp.com/avatars/{userId}/{avatar}.png";
         }
     }
