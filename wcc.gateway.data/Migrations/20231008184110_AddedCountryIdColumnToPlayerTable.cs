40071d86dfa7b1bb9774cc5f16b9789fe421a2de
Yev <str.lucky86@gmail.com>
2023-10-08 14:54:36 -0400

    added CountryId column to Players table.

---
+++ wcc.gateway.data/Migrations/20231008184110_AddedCountryIdColumnToPlayerTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wcc.gateway.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCountryIdColumnToPlayerTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "CountryId",
+                table: "Players",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 232);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountryId",
+                table: "Players");
+        }
+    }
+}
