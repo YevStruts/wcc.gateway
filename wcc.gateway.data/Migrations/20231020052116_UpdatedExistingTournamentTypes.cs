a5638f1a6c421d2202ed735e495d53553d0afff6
Yev <str.lucky86@gmail.com>
2023-11-04 01:25:19 -0400

    added topuranament types

---
+++ wcc.gateway.data/Migrations/20231020052116_UpdatedExistingTournamentTypes.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wcc.gateway.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedExistingTournamentTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+UPDATE Tournament SET [TournamentTypeId] = 4 WHERE Id = 9;
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
